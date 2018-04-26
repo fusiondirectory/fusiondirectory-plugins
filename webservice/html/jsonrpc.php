9d29ddea3cf4e6f706b814845dbf787bbf8b7119
Côme Chilliet <come@opensides.be>
2018-04-26 12:05:59 +0200

    :ambulance: fix(language) Adapt code to new Language class
    issue #5814

--- webservice/html/jsonrpc.php
+++ webservice/html/jsonrpc.php
@@ -72,7 +72,7 @@ function initiateRPCSession($id = NULL, $ldap = NULL, $user = NULL, $pwd = NULL)
     $config = session::global_get('config');
     $plist  = session::global_get('plist');
     $ui     = session::global_get('ui');
-    initLanguage();
+    Language::init();
   } else {
     $config = new config(CONFIG_DIR."/".CONFIG_FILE, $BASE_DIR);
     if ($ldap === NULL) {
