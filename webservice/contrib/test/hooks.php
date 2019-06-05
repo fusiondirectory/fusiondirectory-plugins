040bbe924dbdd4275f84bbac999bbaa9ea58d2f6
Côme Chilliet <come@opensides.be>
2019-06-05 17:35:43 +0200

    :ambulance: fix(webservice) Fix errors spotted by dredd
    Also fixed problems in dredd configuration
    
    issue #5924

--- webservice/contrib/test/hooks.php
+++ webservice/contrib/test/hooks.php
@@ -5,12 +5,16 @@ use Dredd\Hooks;
 $stash = [];
 
 $successfullLogin = '/login > Login to the API > 200 > application/json';
+$logout           = '/logout > End the currently opened session > 204';
+$userCreate       = '/objects/{type} > Create an object of specific type > 201 > application/json';
+$ldapBase         = 'dc=fusiondirectory,dc=org';
 
 Hooks::before($successfullLogin, function(&$transaction) {
-  $requestBody = $transaction->request->body;
+  $requestBody = [];
 
   $requestBody['user']      = 'fd-admin';
   $requestBody['password']  = 'adminpwd';
+
 
   $transaction->request->body = json_encode($requestBody);
 });
@@ -20,7 +24,35 @@ Hooks::after($successfullLogin, function(&$transaction) use (&$stash) {
   $stash['token'] = $parsedBody;
 });
 
-Hooks::beforeEach(function(&$transaction) use (&$stash) {
+Hooks::before($logout, function(&$transaction) {
+  // Skip logout to avoid destroying the session
+  $transaction->skip = TRUE;
+});
+
+Hooks::before($userCreate, function(&$transaction) {
+  $requestBody = [
+    'attrs' => [
+      'user'  => [
+        'givenName' => 'John',
+        'sn' => 'Smith',
+        'uid' => 'jsmith',
+        'userPassword' => 'jsmith',
+      ],
+      'personalInfo' => [
+        'personalTitle' => 'Dr'
+      ]
+    ]
+  ];
+
+  $transaction->request->body = json_encode($requestBody);
+});
+
+Hooks::beforeEach(function(&$transaction) use (&$stash, $ldapBase) {
+  $transaction->fullPath = str_replace(urlencode('dc=example,dc=com'), urlencode($ldapBase), $transaction->fullPath);
+  if (isset($transaction->request->uri)) {
+    $transaction->request->uri = str_replace(urlencode('dc=example,dc=com'), urlencode($ldapBase), $transaction->request->uri);
+  }
+
   if (isset($stash['token']) && ($transaction->expected->statusCode != '401')) {
     $transaction->request->headers->{'Session-Token'} = $stash['token'];
   }
